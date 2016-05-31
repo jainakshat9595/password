6fdee5434d6bd66e7ae20268c1f133bdee84bc03
Akshat Jain <Akshat Jain>
2016-05-31 09:23:39 +0530

    Basic Layout

--- app/src/main/java/in/jainakshat/password/MainApplication.java
+++ app/src/main/java/in/jainakshat/password/MainApplication.java
@@ -3,6 +3,11 @@ package in.jainakshat.password;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import net.soroushjavdan.lockapplicationsample.applicationlockerlibrary.MyLifecycleHandler;
 
@@ -15,6 +20,22 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("appPasscode");
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("dataSnapshot.toString(): "+dataSnapshot.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("databaseError: "+databaseError);
+            }
+        });
+
         registerActivityLifecycleCallbacks(new MyLifecycleHandler("1234"));
     }
 }
